388827f400adb35d9d30f4736d0cb4b731577684
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+using DevExpress.XtraEditors.Repository;
+using DevExpress.XtraGrid.Views.Base;
+using DevExpress.XtraRichEdit;
+
+namespace RichAccessInGridCell {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+
+            InitGridData();
+            AddRepositoryItemToGrid();
+        }
+
+        private void gridView1_ShownEditor(object sender, EventArgs e) {
+            ColumnView columnView = sender as ColumnView;
+            
+            if (columnView != null) {
+                RichTextEdit activeEditor = columnView.ActiveEditor as RichTextEdit;
+                
+                if (activeEditor != null) {
+                    RichEditControl richEditControl = (RichEditControl)activeEditor.Controls[0];
+                    
+                    // TODO: Use any RichEditControl API
+                    richEditControl.ActiveViewType = RichEditViewType.PrintLayout;
+                    richEditControl.ActiveView.ZoomFactor = 2f;
+                    richEditControl.Document.Sections[0].Margins.Left = 50;
+                    richEditControl.Document.Sections[0].Margins.Top = 50;
+                }
+            }
+        }
+
+        #region Helper Methods
+        private void InitGridData() {
+            DataSet ds = new DataSet();
+            DataTable dt = new DataTable("MyDataTable");
+
+            dt.Columns.Add("ID", typeof(Int32));
+            dt.Columns.Add("MyData(RichEdit)", typeof(string));
+            dt.Constraints.Add("IDPK", dt.Columns["ID"], true);
+
+            dt.Rows.Add(new object[] { 0, "Row A" });
+            dt.Rows.Add(new object[] { 1, "Row B" });
+
+            ds.Tables.Add(dt);
+
+            gridControl1.DataSource = ds;
+            gridControl1.DataMember = ds.Tables[0].TableName;
+        }
+
+        private void AddRepositoryItemToGrid() {
+            RepositoryItemRichTextEdit repositoryItemRichTextEdit = new RepositoryItemRichTextEdit();
+
+            gridControl1.RepositoryItems.Add(repositoryItemRichTextEdit);
+            gridView1.Columns["MyData(RichEdit)"].ColumnEdit = repositoryItemRichTextEdit;
+
+            gridView1.RowHeight = 120;
+        }
+        #endregion
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,8 @@
+# Accessing RichEditControl for an active grid cell with RepositoryItemRichTextEdit
+
+
+<p>This capability allows you to use the RichEditControl API for manipulating the cell content that is currently being edited. To accomplish this task you should handle the <a href="http://documentation.devexpress.com/#WindowsForms/DevExpressXtraGridViewsBaseColumnView_ShownEditortopic"><u>ColumnView.ShownEditor Event</u></a>.</p>
+
+<br/>
+
+
