3e182751817fcaa2249c1cf4ab5b9889510b4437
DevExpressExampleBot <fakeEmail>
2023-12-08 17:01:20 +0400

    Source auto update [skip ci]

--- VB/Form1.Designer.vb
+++ VB/Form1.Designer.vb
@@ -1,70 +1,68 @@
-﻿Imports Microsoft.VisualBasic
-Imports System
 Namespace RichAccessInGridCell
-	Partial Public Class Form1
-		''' <summary>
-		''' Required designer variable.
-		''' </summary>
-		Private components As System.ComponentModel.IContainer = Nothing
 
-		''' <summary>
-		''' Clean up any resources being used.
-		''' </summary>
-		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
-			If disposing AndAlso (components IsNot Nothing) Then
-				components.Dispose()
-			End If
-			MyBase.Dispose(disposing)
-		End Sub
+    Partial Class Form1
 
-		#Region "Windows Form Designer generated code"
+        ''' <summary>
+        ''' Required designer variable.
+        ''' </summary>
+        Private components As System.ComponentModel.IContainer = Nothing
 
-		''' <summary>
-		''' Required method for Designer support - do not modify
-		''' the contents of this method with the code editor.
-		''' </summary>
-		Private Sub InitializeComponent()
-			Me.gridControl1 = New DevExpress.XtraGrid.GridControl()
-			Me.gridView1 = New DevExpress.XtraGrid.Views.Grid.GridView()
-			CType(Me.gridControl1, System.ComponentModel.ISupportInitialize).BeginInit()
-			CType(Me.gridView1, System.ComponentModel.ISupportInitialize).BeginInit()
-			Me.SuspendLayout()
-			' 
-			' gridControl1
-			' 
-			Me.gridControl1.Anchor = (CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) Or System.Windows.Forms.AnchorStyles.Left) Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles))
-			Me.gridControl1.Location = New System.Drawing.Point(12, 12)
-			Me.gridControl1.MainView = Me.gridView1
-			Me.gridControl1.Name = "gridControl1"
-			Me.gridControl1.Size = New System.Drawing.Size(552, 333)
-			Me.gridControl1.TabIndex = 0
-			Me.gridControl1.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() { Me.gridView1})
-			' 
-			' gridView1
-			' 
-			Me.gridView1.GridControl = Me.gridControl1
-			Me.gridView1.Name = "gridView1"
-'			Me.gridView1.ShownEditor += New System.EventHandler(Me.gridView1_ShownEditor);
-			' 
-			' Form1
-			' 
-			Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
-			Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-			Me.ClientSize = New System.Drawing.Size(576, 357)
-			Me.Controls.Add(Me.gridControl1)
-			Me.Name = "Form1"
-			Me.Text = "Form1"
-			CType(Me.gridControl1, System.ComponentModel.ISupportInitialize).EndInit()
-			CType(Me.gridView1, System.ComponentModel.ISupportInitialize).EndInit()
-			Me.ResumeLayout(False)
+        ''' <summary>
+        ''' Clean up any resources being used.
+        ''' </summary>
+        ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+            If disposing AndAlso (Me.components IsNot Nothing) Then
+                Me.components.Dispose()
+            End If
 
-		End Sub
+            MyBase.Dispose(disposing)
+        End Sub
 
-		#End Region
+'#Region "Windows Form Designer generated code"
+        ''' <summary>
+        ''' Required method for Designer support - do not modify
+        ''' the contents of this method with the code editor.
+        ''' </summary>
+        Private Sub InitializeComponent()
+            Me.gridControl1 = New DevExpress.XtraGrid.GridControl()
+            Me.gridView1 = New DevExpress.XtraGrid.Views.Grid.GridView()
+            CType((Me.gridControl1), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me.gridView1), System.ComponentModel.ISupportInitialize).BeginInit()
+            Me.SuspendLayout()
+            ' 
+            ' gridControl1
+            ' 
+            Me.gridControl1.Anchor = CType(((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) Or System.Windows.Forms.AnchorStyles.Left) Or System.Windows.Forms.AnchorStyles.Right)), System.Windows.Forms.AnchorStyles)
+            Me.gridControl1.Location = New System.Drawing.Point(12, 12)
+            Me.gridControl1.MainView = Me.gridView1
+            Me.gridControl1.Name = "gridControl1"
+            Me.gridControl1.Size = New System.Drawing.Size(552, 333)
+            Me.gridControl1.TabIndex = 0
+            Me.gridControl1.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() {Me.gridView1})
+            ' 
+            ' gridView1
+            ' 
+            Me.gridView1.GridControl = Me.gridControl1
+            Me.gridView1.Name = "gridView1"
+            AddHandler Me.gridView1.ShownEditor, New System.EventHandler(AddressOf Me.gridView1_ShownEditor)
+            ' 
+            ' Form1
+            ' 
+            Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
+            Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+            Me.ClientSize = New System.Drawing.Size(576, 357)
+            Me.Controls.Add(Me.gridControl1)
+            Me.Name = "Form1"
+            Me.Text = "Form1"
+            CType((Me.gridControl1), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((Me.gridView1), System.ComponentModel.ISupportInitialize).EndInit()
+            Me.ResumeLayout(False)
+        End Sub
 
-		Private gridControl1 As DevExpress.XtraGrid.GridControl
-		Private WithEvents gridView1 As DevExpress.XtraGrid.Views.Grid.GridView
-	End Class
+'#End Region
+        Private gridControl1 As DevExpress.XtraGrid.GridControl
+
+        Private gridView1 As DevExpress.XtraGrid.Views.Grid.GridView
+    End Class
 End Namespace
-
--- VB/Form1.vb
+++ VB/Form1.vb
@@ -1,4 +1,3 @@
-﻿Imports Microsoft.VisualBasic
 Imports System
 Imports System.Data
 Imports System.Windows.Forms
@@ -8,59 +7,51 @@ Imports DevExpress.XtraGrid.Views.Base
 Imports DevExpress.XtraRichEdit
 
 Namespace RichAccessInGridCell
-	Partial Public Class Form1
-		Inherits Form
-		Public Sub New()
-			InitializeComponent()
 
-			InitGridData()
-			AddRepositoryItemToGrid()
-		End Sub
+    Public Partial Class Form1
+        Inherits Form
 
-		Private Sub gridView1_ShownEditor(ByVal sender As Object, ByVal e As EventArgs) Handles gridView1.ShownEditor
-			Dim columnView As ColumnView = TryCast(sender, ColumnView)
+        Public Sub New()
+            InitializeComponent()
+            InitGridData()
+            AddRepositoryItemToGrid()
+        End Sub
 
-			If columnView IsNot Nothing Then
-				Dim activeEditor As RichTextEdit = TryCast(columnView.ActiveEditor, RichTextEdit)
+        Private Sub gridView1_ShownEditor(ByVal sender As Object, ByVal e As EventArgs)
+            Dim columnView As ColumnView = TryCast(sender, ColumnView)
+            If columnView IsNot Nothing Then
+                Dim activeEditor As RichTextEdit = TryCast(columnView.ActiveEditor, RichTextEdit)
+                If activeEditor IsNot Nothing Then
+                    Dim richEditControl As RichEditControl = CType(activeEditor.Controls(0), RichEditControl)
+                    ' TODO: Use any RichEditControl API
+                    richEditControl.ActiveViewType = RichEditViewType.PrintLayout
+                    richEditControl.ActiveView.ZoomFactor = 2F
+                    richEditControl.Document.Sections(0).Margins.Left = 50
+                    richEditControl.Document.Sections(0).Margins.Top = 50
+                End If
+            End If
+        End Sub
 
-				If activeEditor IsNot Nothing Then
-					Dim richEditControl As RichEditControl = CType(activeEditor.Controls(0), RichEditControl)
+'#Region "Helper Methods"
+        Private Sub InitGridData()
+            Dim ds As DataSet = New DataSet()
+            Dim dt As DataTable = New DataTable("MyDataTable")
+            dt.Columns.Add("ID", GetType(Integer))
+            dt.Columns.Add("MyData(RichEdit)", GetType(String))
+            dt.Constraints.Add("IDPK", dt.Columns("ID"), True)
+            dt.Rows.Add(New Object() {0, "Row A"})
+            dt.Rows.Add(New Object() {1, "Row B"})
+            ds.Tables.Add(dt)
+            gridControl1.DataSource = ds
+            gridControl1.DataMember = ds.Tables(0).TableName
+        End Sub
 
-					' TODO: Use any RichEditControl API
-					richEditControl.ActiveViewType = RichEditViewType.PrintLayout
-					richEditControl.ActiveView.ZoomFactor = 2f
-					richEditControl.Document.Sections(0).Margins.Left = 50
-					richEditControl.Document.Sections(0).Margins.Top = 50
-				End If
-			End If
-		End Sub
-
-		#Region "Helper Methods"
-		Private Sub InitGridData()
-			Dim ds As New DataSet()
-			Dim dt As New DataTable("MyDataTable")
-
-			dt.Columns.Add("ID", GetType(Int32))
-			dt.Columns.Add("MyData(RichEdit)", GetType(String))
-			dt.Constraints.Add("IDPK", dt.Columns("ID"), True)
-
-			dt.Rows.Add(New Object() { 0, "Row A" })
-			dt.Rows.Add(New Object() { 1, "Row B" })
-
-			ds.Tables.Add(dt)
-
-			gridControl1.DataSource = ds
-			gridControl1.DataMember = ds.Tables(0).TableName
-		End Sub
-
-		Private Sub AddRepositoryItemToGrid()
-			Dim repositoryItemRichTextEdit As New RepositoryItemRichTextEdit()
-
-			gridControl1.RepositoryItems.Add(repositoryItemRichTextEdit)
-			gridView1.Columns("MyData(RichEdit)").ColumnEdit = repositoryItemRichTextEdit
-
-			gridView1.RowHeight = 120
-		End Sub
-		#End Region
-	End Class
+        Private Sub AddRepositoryItemToGrid()
+            Dim repositoryItemRichTextEdit As RepositoryItemRichTextEdit = New RepositoryItemRichTextEdit()
+            gridControl1.RepositoryItems.Add(repositoryItemRichTextEdit)
+            gridView1.Columns("MyData(RichEdit)").ColumnEdit = repositoryItemRichTextEdit
+            gridView1.RowHeight = 120
+        End Sub
+'#End Region
+    End Class
 End Namespace
